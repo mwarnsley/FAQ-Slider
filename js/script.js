db88936f7b5bbea286e4543f1c91fb5de32adfa5
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-05-12 19:53:19 -0400

    Editing the arrows to rotate when the slider is active and not active

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@
 
 $(document).ready(function() {
     
+    //Setting the variables to use for the slider
     var action = "click";
     var speed = 500;
     
    $("li.q").on(action, function(){
        $(this).next().slideToggle(speed).siblings("li.a").slideUp();
+       
+       //Getting the image for the question
+       var img = $(this).children("img");
+       //Remove the rotate class for all arrow images except the active
+       $("img").not(img).removeClass("rotate");
+       //Toggle the rotate class
+       img.toggleClass("rotate");
    });
     
 });
